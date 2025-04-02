fd160a3d24a85aa810fc2891c9951c04f74aec9d
preston <preston@erissoft.com>
2025-04-02 14:08:51 -0400

    some debugging

--- naiveOrderBook.cpp
+++ naiveOrderBook.cpp
@@ -11,7 +11,7 @@ struct Order {
 
     // Constructor
     Order(int id, double price, int quantity, string side)
-    : id(id), price(price), quantity(quantity), side(side) {}
+    : id(id), price(price), quantity(quantity), type(side) {}
 };
 
 struct OrderList {
@@ -25,23 +25,57 @@ struct OrderList {
 class OrderBook {
     private:
         // Needed for order ID
-        int orderCount = 0;
+        int orderCount;
         OrderList* buyOrderList;
         OrderList* sellOrderList;
+
+        OrderList* searchOrderList(int searchID, OrderList* orderList) {
+            if (!orderList->order && !orderList->next) {
+                return nullptr;
+            }
+            // Create reference to current level in linked list
+            // Both buy and sell have to be searched
+            OrderList* currentLayer = orderList;
+            
+            // Create reference to previous layer
+            OrderList* prevLayer = nullptr;
+
+            // Search for ID
+            while (currentLayer->order->id != searchID && currentLayer->next) {
+                prevLayer = currentLayer;
+                currentLayer = currentLayer->next;
+            }
+
+            // Return either located order list or nullptr
+            if (currentLayer->order->id != searchID && !currentLayer->next) {
+                return nullptr;
+            }
+            return prevLayer;
+        }
 
     public:
         // Constructor
         OrderBook() {
             // Create initial order lists
-            OrderList* buyOrderList = new OrderList(nullptr);
-            OrderList* sellOrderList = new OrderList(nullptr);
+            buyOrderList = new OrderList(nullptr);
+            sellOrderList = new OrderList(nullptr);
+
+            // Declare orderCount value
+            orderCount = 0;
         }
 
         // Destructor
         ~OrderBook() {
-            // Delete order lists allocated using new
+            // Delete order lists and orders allocated using new
+            if (buyOrderList->order) {
+                delete buyOrderList->order;
+            }
             delete buyOrderList;
+            if (sellOrderList->order) {
+                delete sellOrderList->order;
+            }
             delete sellOrderList;
+            // Rest will delete recursively 
         }
 
         // Creates order in linked list
@@ -56,36 +90,42 @@ class OrderBook {
                 return;
             }
 
-            if (type == "BUY" && buyOrderList.order == nullptr) {
+            if (type == "BUY" && !buyOrderList->order) {
                 // Add into empty buy order list
-                buyOrderList.order = new Order(orderCount, price, quantity, type);
-            } else if (type == "SELL" && sellOrderList.order == nullptr) {
+                buyOrderList->order = new Order(orderCount, price, quantity, type);
+            } else if (type == "SELL" && !sellOrderList->order) {
                 // Add into empty sell order list
-                sellOrderList.order = new Order(orderCount, price, quantity, type);
+                sellOrderList->order = new Order(orderCount, price, quantity, type);
             } else {
                 // Create reference to current level in linked list
+                OrderList* currentLayer = nullptr;
                 if (type == "BUY") {
                     OrderList* currentLayer = buyOrderList;
-                } else (type == "SELL") {
+                } else if (type == "SELL") {
                     OrderList* currentLayer = sellOrderList;
                 }
 
                 // Find proper place for next order
-                while (type == "BUY" && price <= currentLayer.order->price && currentLayer.next != nullptr ||
-                       type == "SELL" && price >= currentLayer.order->price && currentLayer.next != nullptr) {
-                        currentLayer = currentLayer.next;
+                while (type == "BUY" && price <= currentLayer->order->price && currentLayer->next ||
+                       type == "SELL" && price >= currentLayer->order->price && currentLayer->next) {
+                        currentLayer = currentLayer->next;
                        }
                 
                 // Create order pointer
-                Order* newOrder = new Order(orderCount, price, quantity, type)
+                Order* newOrder = new Order(orderCount, price, quantity, type);
+
+                cout << "test" << endl;
                 // Create next layer and insert order if needed
-                if (currentLayer.next == nullptr) {
-                    currentLayer.next = new OrderList(orderCount, price, quantity, type);
+                if (!currentLayer->next) {
+                    currentLayer->next = new OrderList(newOrder);
+                } else {
+                    currentLayer->next->order = newOrder;
                 }
+                cout << "test2" << endl;
             }
 
             // Print Order ID
-            cout << "Order Successfully Added. Order ID is " << orderCount << "." << endl;
+            cout << "Order of type " << type << " for " << quantity << " units for price " << price << " successfully added. Order ID is " << orderCount << "." << endl;
 
             // Update counter for order ID
             orderCount += 1;
@@ -93,52 +133,44 @@ class OrderBook {
         
         // Removes order from linked list
         // type should be "BUY" for buy, "SELL" for sell
-        void removeOrder(int id, string type) {
+        void removeOrder(int id) {
             // Return if order type is invalid or quanity is invalid
-            if (type != "BUY" && type != "SELL") {
-                cout << "Order Book Error: Invalid Order Type" << endl;
+            if (id > orderCount) {
+                cout << "Order Book Error: Invalid Order ID" << endl;
                 return;
             }
 
-            // Create reference to current level in linked list
-            if (type == "BUY") {
-                OrderList* currentLayer = buyOrderList;
-            } else (type == "SELL") {
+            // Check buy
+            OrderList* currentLayer = buyOrderList;
+            OrderList* prevLayer = searchOrderList(id, buyOrderList);
+
+            if (!prevLayer) {
+                // Check sell
                 OrderList* currentLayer = sellOrderList;
-            }
-            
-            // Create reference to previous layer
-            OrderList* prevLayer = nullptr;
+                OrderList* prevLayer = searchOrderList(id, sellOrderList);
 
-            // Search for ID
-            while (currentLayer.order.id != id && currentLayer.next != nullptr) {
-                prevLayer = currentLayer;
-                currentLayer = currentLayer.next;
+                if (!prevLayer) {
+                    cout << "Order Book Error: Order ID " << id << " Not Found" << endl;
+                    return;
+                }
             }
 
-            // Catch error of order not being found
-            if (currentLayer.order.id != id && currentLayer.next == nullptr) {
-                cout << "Order Book Error: Order ID Not Found" << endl;
-                return;
-            }
+            // Get currentLayer out of prevLayer
+            currentLayer = prevLayer->next;
 
-            if (currentLayer.next == nullptr) {
+            // Delete dynamically allocated order
+            delete currentLayer->order;
+
+            if (!currentLayer->next) {
                 // Bottom layer no longer needed
                 currentLayer = prevLayer;
-            } else if (prevLayer == nullptr) {
+            } else if (!prevLayer) {
                 // Top layer no longer needed
-                currentLayer = currentLayer.next;
+                currentLayer = currentLayer->next;
             } else {
                 // Get rid of current layer order
-                prevLayer.next = currentLayer.next;
+                prevLayer->next = currentLayer->next;
                 currentLayer = prevLayer;
-            }
-
-            // Update order lists
-            if (type == "BUY") {
-                buyOrderList = currentLayer;
-            } else (type == "SELL") {
-                sellOrderList = currentLayer;
             }
 
             // Print Status
@@ -147,23 +179,33 @@ class OrderBook {
 
         // Match buy and sell orders
         void matchOrders() {
-            while (buyOrderList->order->price > sellOrderList->order->price) {
+            while (buyOrderList->order && sellOrderList->order && buyOrderList->order->price >= sellOrderList->order->price) {
                 double orderPrice = buyOrderList->order->price;
                 int orderQuantity = min(buyOrderList->order->quantity, sellOrderList->order->quantity);
                 if (buyOrderList->order->quantity == orderQuantity && sellOrderList->order->quantity == orderQuantity) {
                     // Edge case
-                    removeOrder(buyOrderList->order->id, "BUY");
-                    removeOrder(sellOrderList->order->id, "SELL");
+                    removeOrder(buyOrderList->order->id);
+                    removeOrder(sellOrderList->order->id);
                 } else if (buyOrderList->order->quantity == orderQuantity) {
                     // Delete one and remove from larger
-                    removeOrder(buyOrderList->order->id, "BUY");
+                    removeOrder(buyOrderList->order->id);
                     sellOrderList->order->quantity -= orderQuantity;
                 } else if (sellOrderList->order->quantity == orderQuantity) {
                     // Delete one and remove from larger
-                    removeOrder(sellOrderList->order->id, "SELL");
+                    removeOrder(sellOrderList->order->id);
                     buyOrderList->order->quantity -= orderQuantity;
                 }
                 cout << orderQuantity << " units sold for $" << orderPrice << "." << endl;
             }
         }
+};
+
+int main() {
+    OrderBook orderBook = OrderBook();
+    orderBook.addOrder(100.0, 10, "BUY");
+    orderBook.addOrder(90.0, 5, "SELL");
+    orderBook.addOrder(125.0, 15, "BUY");
+    // orderBook.removeOrder(2);
+    // orderBook.matchOrders();
+    return 0;
 }

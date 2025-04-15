079c93cb11b5daddf6786ccdce531dc8b9866eb5
Felforge <preston@erissoft.com>
2025-04-14 22:09:28 -0400

    Start addOrder

--- cpuOrderBook/cpuOrderBook.cpp
+++ cpuOrderBook/cpuOrderBook.cpp
@@ -2,15 +2,39 @@
 #include "cpuOrderBook.h"
 using namespace std;
 
+Order::Order(int id, string side, string type, int quantity, double price)
+    : id(id), side(side), type(type), quantity(quantity), price(price) {}
+
+PriceLevel::PriceLevel(Order* order) : order(order), prev(nullptr), next(nullptr) {}
+
+Ticker::Ticker(string ticker) : ticker(ticker), bestBuyOrder(nullptr), bestSellOrder(nullptr) {}
+
 // Constructor
 OrderBook::OrderBook() {
-
+    orderCount = 0;
 }
 
-int main() {
-    OrderBook orderBook = OrderBook();
-    if (orderBook.buyOrderList[0] == nullptr) {
-        cout << "test" << endl;
+void OrderBook::addOrder(string side, string type, int quantity, double price, bool print, bool execute) {
+    // Check for all errors
+    if (side != "BUY" && side != "SELL") {
+        cout << "Order Book Error: Invalid Order Side" << endl;
+        return;
+    } else if (quantity <= 0) { 
+        cout << "Order Book Error: Quantity Must Be An Integer Greater Than 0" << endl;
+        return;
+    } else if (price <= 0.0) {
+        cout << "Order Book Error: Price Must Be A Number Greater Than 0" << endl;
+        return;
+    } else if (type != "MARKET" && type != "LIMIT" && type != "STOP") {
+        cout << "Order Book Error: Invalid Order Type" << endl;
+        return;
     }
-    return 0;
 }
+
+// int main() {
+//     OrderBook orderBook = OrderBook();
+//     if (orderBook.buyOrderList[0] == nullptr) {
+//         cout << "test" << endl;
+//     }
+//     return 0;
+// }
--- cpuOrderBook/cpuOrderBook.h
+++ cpuOrderBook/cpuOrderBook.h
@@ -2,19 +2,31 @@
 #define CPUORDERBOOK_H
 
 #include <string>
+#include <unordered_map>
 
 struct Order {
     int id;
     double price;
     int quantity;
     std::string type;
-    Order(int id, double price, int quantity, std::string side);
+    std::string side;
+    Order(int id, std::string side, std::string type, int quantity, double price);
 };
 
 struct PriceLevel {
-    Order order;
+    Order* order;
     PriceLevel* prev;
     PriceLevel* next;
+    PriceLevel(Order* order);
+};
+
+struct Ticker {
+    std::string ticker;
+    PriceLevel* buyOrderList[100000];
+    PriceLevel* sellOrderList[100000];
+    PriceLevel* bestBuyOrder; // pointer to current best buy order
+    PriceLevel* bestSellOrder; // pointer to current best sell order
+    Ticker(std::string ticker);
 };
 
 class OrderBook {
@@ -22,9 +34,12 @@ class OrderBook {
         int orderCount;
 
     public:
-        PriceLevel* buyOrderList[100000];
-        PriceLevel* sellOrderList[100000];
+        // Easy access to every order
+        // Just make sure to erase from here if the order gets deleted
+        std::unordered_map<int, PriceLevel*> orderMap;
         OrderBook();
-};
+        ~OrderBook();
+        void addOrder(std::string side, std::string type, int quantity, double price = 0.0, bool print = true, bool execute = true);
+    };
 
 #endif

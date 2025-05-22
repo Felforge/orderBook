e9e93f52706bc5fc9eb2d3c3422c8c78f7354302
Felforge <preston@erissoft.com>
2025-05-21 20:59:34 -0400

    Sundell-Tsigas

--- lockless/queue/queue.h
+++ lockless/queue/queue.h
@@ -2,84 +2,83 @@
 #define QUEUE_H
 
 #include <atomic>
+#include <cstdint>
+#include <cstddef>
 
-// List Node
-// memoryBlock is for memory pool allocation
+// Based off of an algorithm developed by Sundell and Tsigas
+
+// Predeclare for access below
 template<typename T>
-struct alignas(64) Node {
-    T data;
-    void* memoryBlock;
-    std::atomic<Node*> prev;
-    std::atomic<Node*> next;
-    Node(T data, void* memoryBlock=nullptr) : data(data), memoryBlock(memoryBlock) {
-        prev.store(nullptr);
-        next.store(nullptr);
-    }
+struct Node;
+
+// We use pointer tagging for marking deletion, so all pointers must be at least 2-byte aligned
+template<typename T>
+class MarkedPtr {
+    private:
+        static constexpr uintptr_t MARK_MASK = 1;
+        uintptr_t bits;
+
+    public:
+        // Used if no parameters are specified
+        MarkedPtr() : bits(0) {}
+
+        // Used if parameters are specified
+        MarkedPtr(Node<T>* ptr, bool mark) : bits((uintptr_t)ptr | (mark ? MARK_MASK : 0)) {}
+
+        // Marked as const as to not modify any member variables of the parent class
+        Node<T>* getPtr() const {
+            // ~MARK_MASK inverts tje bits of MARK_MASK
+            // The return statement clears the first bit and casts it to Node<T>*
+            return (Node<T>*)(bits & ~MARK_MASK);
+        }
+        
+        // Marked as const as to not modify any member variables of the parent class
+        bool getMark() const {
+            // Isolates the lowest bit
+            // If the result is nonzero it means the mark is set and returns true
+            return bits & MARK_MASK;
+        }
+
+        // Comparison operators for MakredPtr objects
+        // Marked as const as to not modify any member variables of the parent class
+        bool operator==(const MarkedPtr& other) const {
+            return bits == other.bits;
+        }
+        bool operator!=(const MarkedPtr& other) const {
+            return !(*this == other);
+        }
 };
 
-template<typename NodeType>
-class Queue {
+// Node of doubly linked list
+// memoryBlock is for memory pool allocation
+template<typename T>
+struct Node {
+    std::atomic<MarkedPtr<T>> prev;
+    std::atomic<MarkedPtr<T>> next;
+    T* data;
+
+    // Number of threads that are currently holding a reference to this node
+    std::atomic<size_t> refCount;
+
+    // Track if this is a dummy node or not
+    bool isDummy;
+
+    // For memory pool allocation if applicable
+    void* memoryBlock;
+
+    // Used if no parameters are provided
+    Node() : prev(MarkedPtr<T>(nullptr, false)), next(MarkedPtr<T>(nullptr, false)), 
+        data(nullptr), refcount(1), is_dummy(true), memoryBlock(nullptr) {}
+    
+    // Used if parameters are provided
+    Node(void* memoryBlock, const T& val) : prev(MarkedPtr<T>(nullptr, false)), next(MarkedPtr<T>(nullptr, false)), 
+        data(val), refcount(1), value(val), is_dummy(false), memoryBlock(memoryBlock) {}
+};
+
+template<typename T>
+class LocklessQueue {
     public:
-
-        // Head and tail pointers
-        std::atomic<NodeType*> head;
-        std::atomic<NodeType*> tail;
-
-        Queue() {
-            // Initialize head and tail
-            head.store(nullptr);
-            tail.store(nullptr);
-        }
-
-        virtual void insert(NodeType* nodePtr) {
-            while(true) {
-                NodeType* headPtr = head.load();
-                NodeType* tailPtr = tail.load();
-                if (!headPtr) {
-                    // List is empty, set both head and tail to newNode
-                    if (head.compare_exchange_weak(headPtr, nodePtr)) {
-                        tail.store(nodePtr);
-                        return;
-                    }
-                } else if (tailPtr) {
-                    // Update the next pointer of the current tail
-                    NodeType* nullNode = nullptr;
-                    if (tailPtr->next.compare_exchange_weak(nullNode, nodePtr)) {
-                        // Successfully updated tail's next, now update tail
-                        nodePtr->prev.store(tailPtr);
-                        tail.store(nodePtr);
-                        return;
-                    }
-                }
-                 // Retry
-            }
-        }
-
-        // Removes head
-        virtual NodeType* remove(NodeType* nodePtr = nullptr) {
-            NodeType* nullNode = nullptr;
-            NodeType* headNext;
-
-            while (true) {
-                NodeType* headPtr = head.load();
-                if (!headPtr) {
-                    return nullptr;
-                } 
-                headNext = headPtr->next.load();
-                if (headNext) {
-                    if (headNext->prev.compare_exchange_weak(headPtr, nullNode)) {
-                        head.store(headNext);
-                        return headPtr;
-                    }
-                } else {
-                    if (head.compare_exchange_weak(headPtr, nullNode)) {
-                        tail.store(nullptr);
-                        return headPtr;
-                    }
-                }
-                // Failed, retry
-            }
-        }
+    private:
 };
 
 #endif

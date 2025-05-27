345e5b327e2d8d7cf88556f83d403353888bd44c
Felforge <preston@erissoft.com>
2025-05-26 22:45:48 -0400

    Continue upgrading lockless queue

--- lockless/queue/queue.h
+++ lockless/queue/queue.h
@@ -8,11 +8,11 @@
 // Based off of an algorithm developed by Sundell and Tsigas
 
 // Predeclare for access below
-template<typename T>
+template <typename T>
 struct Node;
 
 // We use pointer tagging for marking deletion, so all pointers must be at least 2-byte aligned
-template<typename T>
+template <typename T>
 class MarkedPtr {
     private:
         static constexpr uintptr_t MARK_MASK = 1;
@@ -51,7 +51,7 @@ class MarkedPtr {
 
 // Node of doubly linked list
 // memoryBlock is for memory pool allocation
-template<typename T>
+template <typename T>
 struct Node {
     std::atomic<MarkedPtr<T>> prev;
     std::atomic<MarkedPtr<T>> next;
@@ -74,6 +74,76 @@ struct Node {
     Node(void* memoryBlock, const T& val) : prev(MarkedPtr<T>(nullptr, false)), next(MarkedPtr<T>(nullptr, false)), 
         data(val), refcount(1), value(val), is_dummy(false), memoryBlock(memoryBlock) {}
 };
+
+// Returns a node pointer and incraments the reference count
+template <typename T>
+Node<T>* copy(Node<T>* node) {
+    if (node) {
+    // Incrament counter while avoiding contention
+        node->refCount.fetch_add(1, std::memory_order_relaxed);
+    }
+    return node;
+}
+
+// Predeclare for usage in terminateNode
+template <typename T>
+void releaseNode(Node<T>* node);
+
+template <typename T>
+void terminateNode(Node<T>* node) {
+    if (!node) {
+        return;
+    }
+    releaseNode<T>(node->prev.load().getPtr());
+    releaseNode<T>(node->next.load().getPtr());
+
+    // Handle memory block if needed
+    if (node->memoryBlock) {
+        // Free memory
+    } else {
+        delete node;
+    }
+}
+
+template <typename T>
+void releaseNode(Node<T>* node) {
+    if (!node) {
+        return;
+    }
+    // Only reference is being deleted so node can also be terminated
+    if (node->refCount.fetch_sub(1, std::memory_order_acq_rel) == 1) {
+        terminateNode<T>(node);
+    }
+}
+
+// Increments the node's reference count so that the node won't be deleted while being used
+// Returns nullptr if mark is set and returns node pointer otherwise
+template <typename T>
+Node<T>* deref(std::atomic<MarkedPtr<T>>* ptr) {
+    // Load MarpedPtr object from atomic
+    MarkedPtr<T> mPtr = ptr->load(std::memory_order_acquire);
+    
+    // Return nullptr if mark is set
+    if (mPtr.getMark()) {
+        return nullptr;
+    }
+
+    // Load node from MarkedPtr
+    Node<T>* node = mPtr.getPtr();
+
+    // If node is invalid copy handles it
+    return copy<T>(node);
+}
+
+// Like deref but a pointer is always returned
+template <typename T>
+Node<T>* deref(std::atomic<MarkedPtr<T>>* ptr) {
+    // Load node from atomic MarkedPtr
+    Node<T>* node = ptr->load(std::memory_order_acquire).getPtr();
+
+    // If node is invalid copy handles it
+    return copy<T>(node);
+}
 
 template<typename T>
 class LocklessQueue {

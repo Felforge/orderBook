4ef1ba2aff57e6c5b018de0c9a44da4c96a62145
preston <preston@erissoft.com>
2025-07-23 09:16:18 -0400

    Start parallel order book deque implementation

--- parallelOrderBook/parallelOrderBook.h
+++ parallelOrderBook/parallelOrderBook.h
@@ -9,99 +9,92 @@
 #include <thread>
 #include <vector>
 #include "../memoryPool/memoryPool.h"
-#include "../lockless/orderList/orderList.h"
 #include "../lockless/queue/queue.h"
-#include "../lockless/priorityQueue/priorityQueue.h"
 
-const int MAX_THREADS = 8; // Laptop
-// const int MAX_THREADS = 24; // PC
+// NUM_THREADS will be set to the maximum supported amount
+const int NUM_THREADS = std::thread::hardware_concurrency();
 
-// For buy requests
-struct alignas(64) BuyRequest {
+// Order struct
+struct Order {
+    // For memory pool
     void* memoryBlock;
-    OrderNode* orderNode;
-    void* levelBlock;
-    void* queueBlock;
-    std::atomic<BuyRequest*> prev;
-    std::atomic<BuyRequest*> next;
-    BuyRequest(void* memoryBlock, OrderNode* orderNode, void* levelBlock, void* queueBlock);
-};
 
-// For remove requests
-struct alignas(64) RemoveRequest {
-    void* memoryBlock;
-    int ID;
-    std::atomic<RemoveRequest*> prev;
-    std::atomic<RemoveRequest*> next;
-    RemoveRequest(void* memoryBlock, int ID);
-};
+    // Unique identifier
+    int orderID;
+
+    // User/client ID
+    int userID;
+
+    // Order price
+    double price;
+
+    // Order quantity
+    int quantity;
+
+    // "BUY" or "SELL"
+    std::string side;
+
+    // Symbol name
+    std::string ticker;
+
+    // Constructor matching cpuOrderBook interface
+    Order(void* memoryBlock, int orderID, int userID, std::string side,
+        std::string ticker, int quantity, double price);
+  };
 
 // Best orders are being left as PriceLevel to access the whole doubly linked list
 struct alignas(64) Ticker {
     void* memoryBlock;
     std::string ticker;
-    std::vector<OrderList*> buyOrderList;
-    std::vector<OrderList*> sellOrderList;
-    std::atomic<int> bestBuyIdx; // List index of best buy order
-    std::atomic<int> bestSellIdx; // List index of best sell order
     
-    // Priority queues for active price levels
-    std::vector<bool> activeBuyLevels; // Track active buy price levels
-    std::vector<bool> activeSellLevels; // Track active sell price levels
-    PriorityQueue priorityBuyPrices; // Queue of buy prices
-    PriorityQueue prioritySellPrices; // Reverse queue of sell prices
+    // Only accessed by a single thread
+    std::unordered_map<double, Order*> buyOrderMap;
+    std::unordered_map<double, Order*> sellOrderMap;
+
+    // Atomic for cross-thread reads
+    std::atomic<double> bestBuyPrice{0.0};
+    std::atomic<double> bestSellPrice{0.0};
     
-    Ticker(void* memoryBlock, std::string ticker, int numLevels);
+    Ticker(void* memoryBlock, std::string ticker) : memoryBlock(memoryBlock), ticker(ticker) {}
 };
 
+// Struct to hold all memory pools for a given thread
+struct alignas(64) Pools {
+
+};
+
+template<size_t maxTickers>
 class OrderBook {
     private:
-        int orderID;
-        int maxTickers; // Max number of tickers that can be added
-        int maxOrders; // Max number of orders that can be added
-        double minPrice; // Minimum price available for orders
-        double maxPrice; // Maximum price available for orders
-        int numLevels; // Number of available price levels
-        MemoryPool orderPool; // Memory pool for allocating Orders
-        MemoryPool nodePool; // Memory pool for allocating OrderNodes
-        MemoryPool priceLevelPool; // Memory pool for allocation PriceLevels
-        MemoryPool tickerPool; // Memory pool for allocating Tickers
-        MemoryPool buyRequestPool; // Memory pool for allocating buy requests
-        MemoryPool removeRequestPool; // Memory pool for allocating remove requests
-        MemoryPool priceQueuePool; // Memory pool for allocating price queue nodes
+        // Universal order ID
+        std::atomic<int> orderID{0};
 
-        // Hold threads
-        std::vector<std::thread> threads;
+        // Hold matching threads
+        // One thread per ticker
+        std::thread matchingThreads[maxTickers];
 
-        // Requests
-        Queue<BuyRequest> buyQueue;
-        Queue<RemoveRequest> sellQueue;
+        // Orders to be tracked
+        LocklessQueue<Order> toTrack;
 
-        // For order lists
-        int getListIdx(double price);
+        // Map to hold all deques
+        std::unordered_map<std::string, LocklessQueue<Order>*> symbolQueues;
 
-        // For multithreading
-        bool running;
-        void startup();
-        void shutdown();
-        void receiveRequests();
-        void processBuy(BuyRequest* nodePtr);
-        void processSell(RemoveRequest* nodePtr);
+        // Thread function to intake a given order
+        void intakeOrder(int userID, std::string ticker, std::string side, int quantity, double price) {
+            // Create order object
+            Order* order = nullptr; // Add createOrder later
 
-        // Utility functions
-        void updateBestIdx(PriorityQueue &queue, std::vector<bool> &activeLevels, std::atomic<int> &bestIdx);
+            // Add order to be tracked
+            toTrack.pushRight(order, nullptr); // Add thread local memory pool later
+
+            // Add order to order book
+            symbolQueues[ticker]->pushRight(order, nullptr); // Add thread local memory pool later
+        }
 
     public:
-        // Easy access to every order
-        // Just make sure to erase from here if the order gets deleted
-        std::vector<OrderNode*> orders;
-        std::unordered_map<std::string, Ticker*> tickerMap;
         OrderBook(int numTickers, int maxOrders, double inpMinPrice, double inpMaxPrice);
         ~OrderBook();
-        void addTicker(std::string ticker);
         void addOrder(int userID, std::string ticker, std::string side, int quantity, double price, bool print = true);
-        void removeOrder(int id, bool print = true);
-        void matchOrders(std::string ticker, bool print=true, int count=0);
 
         // For testing
         std::atomic<int> ordersProcessed;
